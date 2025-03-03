4ee9fda44201cef1cde43018c559ec4b13fce326
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-03-03 20:09:09 +0530

    Fixerd Card error and navbar

--- src/components/projectCards.tsx
+++ src/components/projectCards.tsx
@@ -7,6 +7,7 @@ const ProjectCards = () => {
     <div id="projects" className="grid gap-6 w-full max-w-5xl sm:grid-cols-3">
       {projectsData.map((project) => (
         <Card
+          key={project.name}
           title={project.title}
           description={project.description}
           image="https://picsum.photos/300/200"
